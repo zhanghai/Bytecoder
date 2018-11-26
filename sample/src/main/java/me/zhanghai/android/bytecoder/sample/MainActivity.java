647d26983f3974c240701a5393fd0bc90328fe50
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-11-26 01:09:01 -0800

    [Feature] Use TextView for sample.

--- sample/src/main/java/me/zhanghai/android/bytecoder/sample/MainActivity.java
+++ sample/src/main/java/me/zhanghai/android/bytecoder/sample/MainActivity.java
@@ -7,7 +7,8 @@ package me.zhanghai.android.bytecoder.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import me.zhanghai.android.bytecoder.library.InvokeStatic;
 import me.zhanghai.android.bytecoder.library.TypeName;
 
@@ -19,7 +20,11 @@ public class MainActivity extends Activity {
 
         Object activityThread = ActivityThread_currentActivityThread();
         String text = "ActivityThread.currentActivityThread(): " + activityThread.toString();
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        setContentView(textView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
     @InvokeStatic(className = "android.app.ActivityThread", methodName = "currentActivityThread")
