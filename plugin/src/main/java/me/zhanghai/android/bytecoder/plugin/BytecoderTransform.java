dda927d5afad7a5daef759a2cdec1d9e73842c8d
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-11-26 21:41:17 -0800

    [Feature] Make transform incremental.

--- plugin/src/main/java/me/zhanghai/android/bytecoder/plugin/BytecoderTransform.java
+++ plugin/src/main/java/me/zhanghai/android/bytecoder/plugin/BytecoderTransform.java
@@ -8,11 +8,12 @@ package me.zhanghai.android.bytecoder.plugin;
 import com.android.build.api.transform.DirectoryInput;
 import com.android.build.api.transform.Format;
 import com.android.build.api.transform.QualifiedContent;
+import com.android.build.api.transform.Status;
 import com.android.build.api.transform.Transform;
-import com.android.build.api.transform.TransformException;
 import com.android.build.api.transform.TransformInput;
 import com.android.build.api.transform.TransformInvocation;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -20,6 +21,7 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 
 public class BytecoderTransform extends Transform {
@@ -41,60 +43,89 @@ public class BytecoderTransform extends Transform {
 
     @Override
     public boolean isIncremental() {
-        // TODO: Support incremental.
-        return false;
+        return true;
     }
 
     @Override
-    public void transform(TransformInvocation transformInvocation) throws TransformException,
-            InterruptedException, IOException {
+    public void transform(TransformInvocation transformInvocation) throws IOException {
 
         Path outputDirectory = transformInvocation.getOutputProvider().getContentLocation(getName(),
                 getOutputTypes(), getScopes(), Format.DIRECTORY).toPath();
-        if (Files.exists(outputDirectory)) {
-            Files.walkFileTree(outputDirectory, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attributes)
-                        throws IOException {
-                    Files.delete(file);
-                    return FileVisitResult.CONTINUE;
-                }
-                @Override
-                public FileVisitResult postVisitDirectory(Path directory, IOException exception)
-                        throws IOException {
-                    if (exception != null) {
-                        throw exception;
-                    }
-                    Files.delete(directory);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
+        if (!transformInvocation.isIncremental()) {
+            deleteRecursivelyIfExists(outputDirectory);
+            Files.createDirectories(outputDirectory);
         }
-        Files.createDirectories(outputDirectory);
 
         for (TransformInput input : transformInvocation.getInputs()) {
             for (DirectoryInput directoryInput : input.getDirectoryInputs()) {
                 Path inputDirectory = directoryInput.getFile().toPath();
-                Files.walkFileTree(inputDirectory, new SimpleFileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult preVisitDirectory(Path directory,
-                                                             BasicFileAttributes attributes)
-                            throws IOException {
-                        Path directoryInOutput = outputDirectory.resolve(inputDirectory.relativize(
-                                directory));
-                        Files.createDirectories(directoryInOutput);
-                        return FileVisitResult.CONTINUE;
+                if (transformInvocation.isIncremental()) {
+                    for (Map.Entry<File, Status> fileStatusEntry
+                            : directoryInput.getChangedFiles().entrySet()) {
+                        Path path = fileStatusEntry.getKey().toPath();
+                        Status status = fileStatusEntry.getValue();
+                        switch (status) {
+                            case NOTCHANGED:
+                                break;
+                            case ADDED:
+                            case CHANGED:
+                                transformPath(path, inputDirectory, outputDirectory);
+                                break;
+                            case REMOVED:
+                                deleteRecursivelyIfExists(path);
+                                break;
+                            default:
+                                throw new IllegalArgumentException();
+                        }
                     }
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-                            throws IOException {
-                        Path fileInOutput = outputDirectory.resolve(inputDirectory.relativize(
-                                file));
-                        BytecoderClassTranformer.transform(file, fileInOutput);
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
+                } else {
+                    transformPath(inputDirectory, inputDirectory, outputDirectory);
+                }
             }
         }
+    }
+
+    private void transformPath(Path path, Path inputDirectory, Path outputDirectory)
+            throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attributes)
+                    throws IOException {
+                Path directoryInOutput = outputDirectory.resolve(inputDirectory.relativize(
+                        directory));
+                Files.createDirectories(directoryInOutput);
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                    throws IOException {
+                Path fileInOutput = outputDirectory.resolve(inputDirectory.relativize(file));
+                BytecoderClassTranformer.transform(file, fileInOutput);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private static void deleteRecursivelyIfExists(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            return;
+        }
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attributes)
+                    throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult postVisitDirectory(Path directory, IOException exception)
+                    throws IOException {
+                if (exception != null) {
+                    throw exception;
+                }
+                Files.delete(directory);
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 }
